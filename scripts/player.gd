517b60e92cb51b54b0673bd2c065ee6d75e0e5f1
MW\HE-0038 <anniehe232@gmail.com>
2025-03-10 19:28:35 +1100

    comments on bugs that need fixing

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,23 +1,75 @@
 extends CharacterBody2D
 
-@export var speed = 500
-@export var gravity = 30
-@export var jump_height = 600
+#HI PLEASE FIX BUG WHERE YOU DASH DURING A JUMP AND THE JUMP CONTINUES AFTER THE DASH
 
-func _physics_process(_delta):
+#export var
+@export var speed = 200 #Movement per frame
+@export var gravity = 30 #Gravitational pull down
+@export var jump_height = 400 #Jump height
+@export var dash = 10 #Dash distance
+@export var direction = -1 #Started facing left, 1 = left, -1 = right
+
+#onready var
+@onready var dash_timer = $Dash_Timer
+
+#other var
+var can_dash = true
+var air_dash_ok = 1
+var dash_active = false
+var can_jump = true
+
+#Test beneath
+func _ready():
+	pass
+
+#Main
+func _physics_process(delta):
 	
-	if !is_on_floor():
+	#Gravity
+	if !is_on_floor() && dash_active == false:
 		velocity.y += gravity
 		if velocity.y > 2000:
 			velocity.y = 2000
 	
-	if Input.is_action_just_pressed("jump") && is_on_floor():
+	#Recharge dash on floor
+	if is_on_floor():
+		air_dash_ok = 1
+	
+	#Regular jump
+	if Input.is_action_just_pressed("jump") && is_on_floor() && can_jump == true:
 		velocity.y = -jump_height
+	
+	#Double jump
+	if Input.is_action_just_pressed("jump") && not is_on_floor():
+		velocity.y = -jump_height + (jump_height * 0.1)
+	
+	#Left/Right movement
 	var horizontal_direction = Input.get_axis("move_left", "move_right")
 	velocity.x = speed * horizontal_direction
 	move_and_slide()
 	
+	#Left/Right direction change
 	if Input.is_action_just_pressed("move_left"):
 		$Player_Sprite.scale.x = 1
+		direction = -1
 	if Input.is_action_just_pressed("move_right"):
 		$Player_Sprite.scale.x = -1
+		direction = 1
+	
+	#Dashing
+	if Input.is_action_just_pressed("dash") && can_dash && air_dash_ok == 1:
+		can_dash = false
+		can_jump = false
+		dash_active = true
+		if !is_on_floor():
+			air_dash_ok = 0
+		var predash_y = position.y
+		velocity.x = dash * direction
+		for i in range(10):
+			position.x += dash * direction
+			dash_timer.start(0.005)
+			await dash_timer.timeout
+			position.y = predash_y
+		can_dash = true
+		can_jump = true
+		dash_active = false
